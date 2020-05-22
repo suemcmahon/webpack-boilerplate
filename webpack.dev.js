36b1dc825ff0c64d62997fead25c13f417bb068c
sue <infosuemcmahon@gmail.com>
2020-05-22 16:40:09 +0100

    Remove HtmlWebpackPlug from prod config dev file.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const SassLintPlugin = require('sass-lint-webpack');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = merge(common, {
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,6 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const SassLintPlugin = require('sass-lint-webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = merge(common, {
