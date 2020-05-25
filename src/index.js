561e7a77c80e3b9d2a949d305198cdb3c9ea8722
sue <infosuemcmahon@gmail.com>
2020-05-25 13:51:53 +0100

    Add Jest for unit tests

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import './sass/style.scss';
-
-const demo = 'bow wow wow!';
-
-console.log(`hello ${demo}`);
---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import '../sass/style.scss';
+const sum = require('./sum');
+const demo = 'bow wow wow!';
+const answer = sum(1,2);
+
+console.log(`hello ${demo}`);
+console.log(`the sum is ${answer}`);
